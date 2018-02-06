3b091e95bd3a86b9e9b2a7a7fb646bc7ec278866
GavinThomas1192 <gthomas1192@gmail.com>
2018-02-06 09:09:53 -0800

    fizzbuz

--- src/App.js
+++ src/App.js
@@ -27,6 +27,34 @@ class App extends Component {
     }
     this.setState({ data: dataArray })
   }
+  // Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
+  fizzBuzz = () => {
+    // for (var i = 0; i <= 100; i++) {
+    //   if (i % 3 == 0 && i % 5 == 0) {
+    //     console.log(i, 'fizzBuzz')
+    //   } else if (i % 5 == 0) {
+    //     console.log(i, 'buzz')
+    //   } else if (i % 3 == 0) {
+    //     console.log(i, 'fizz')
+    //   } else {
+    //     console.log(i)
+    //   }
+    // }
+    var output;
+    for (var i = 1; i <= 100; i++) {
+      output = '';
+      if (i % 3 === 0) {
+        output += 'Fizz';
+      }
+      if (i % 5 === 0) {
+        output += 'Buzz';
+      }
+      if (output === '') {
+        output += i;
+      }
+      console.log(output);
+    }
+  }
 
   render() {
     const data = ['one', 'two', 'three']
@@ -38,7 +66,7 @@ class App extends Component {
 
         <BarChart data={this.state.data} size={[500, 500]} />
 
-        <button onClick={this.randomDataFire}>New Random data</button>
+        <button onClick={this.fizzBuzz}>New Random data</button>
 
       </div>
     );
